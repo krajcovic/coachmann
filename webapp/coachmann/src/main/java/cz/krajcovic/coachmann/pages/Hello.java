fbfd9296630112ca48846ddd6b209e694282c48f
Dusan Krajcovic <dkrajcovic@monetplus.cz>
2012-07-30 09:31:16 +0200

    CLOSED - task 9: Create layout page for all pages. http://github.com/krajcovic/coachmann/issues/issue/9

--- webapp/coachmann/src/main/java/cz/krajcovic/coachmann/pages/Hello.java
+++ webapp/coachmann/src/main/java/cz/krajcovic/coachmann/pages/Hello.java
@@ -1,10 +1,9 @@
 package cz.krajcovic.coachmann.pages;
 
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 
-public class Hello extends WebPage {
+public class Hello extends Layout{
 
 	/**
 	 * 
---
+++ webapp/coachmann/src/main/java/cz/krajcovic/coachmann/pages/Layout.java
@@ -0,0 +1,18 @@
+package cz.krajcovic.coachmann.pages;
+
+import org.apache.wicket.markup.html.WebPage;
+
+public class Layout extends WebPage {
+	
+	//ResourceBundle res;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public Layout() {
+		//res = ResourceBundle.getBundle("TextResources", new Locale("cs"));
+	}
+
+}
